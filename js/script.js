f4c00c9ac0b4317065b8acf6dc22627bb5c74f90
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-02-22 06:03:56 -0800

    interview tab and rejected tab filter done

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ console.log('Hello World!');
 
 let interViewList = [];
 let rejectedList = [];
+let  currentStatus = [];
 
 // count section dhora
 const totalCount= getElementId('total-count');
 const interviewCount= getElementId('interview-count');
 const rejectedCount= getElementId('rejected-count');
-
+const totalCount2 = getElementId('job')
 // button dhora 
 
 const allBtn = getElementId('all');
@@ -31,13 +32,31 @@ function calculateCount(){
     totalCount.innerText = allCards.children.length;
     interviewCount.innerText = interViewList.length;
     rejectedCount.innerText = rejectedList.length;
+//     totalCount2.innerText = allCards.children.length
+
+//     // ekhane interview button a dhukle job count dekhabe
+
+// const jobApplied = interViewList.length + rejectedCount.length
+ 
+// if(totalCount2){
+//     totalCount2.innerText =`
+//     ${jobApplied} of ${totalCount} jobs
+    
+//     `;
 }
+
+
+
+
+
 
 calculateCount()
 
 // button toggle 
 
 function toggleBtn (id){
+
+    currentStatus = id ;
 
     // remove btn-primary from  all button 
     allBtn.classList.remove('btn-primary');
@@ -60,9 +79,18 @@ function toggleBtn (id){
     if(id =='interview'){
         allCards.classList.add('hidden');
         filterSection.classList.remove('hidden');
-    } else if ( id == 'all'){
+         renderInterview();
+
+
+    }else if ( id == 'all'){
         allCards.classList.remove('hidden');
         filterSection.classList.add('hidden');
+       
+    } else if (  id =='rejected'){
+         allCards.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderRejected()
+        
     }
 
 
@@ -73,20 +101,22 @@ function toggleBtn (id){
 mainContainer.addEventListener('click', function(event){
   if(event.target.id === 'interview-btn'){
 
-     const parentNode = event.target.parentNode.parentNode;
+   const parentNode = event.target.parentNode.parentNode;
    const jobName = parentNode.querySelector('#job-name').innerText;
    const jobSkill = parentNode.querySelector('#skill').innerText;
    const jobRemote= parentNode.querySelector('#remote-job').innerText;
    const interviewStatus = parentNode.querySelector('#status').innerText;
    const jobDiscription = parentNode.querySelector('#job-discription').innerText;
 
-   parentNode.querySelector('#status').innerText='Applied';
+   parentNode.querySelector('#status').innerText='Interview';
+
+    
 
    const jobInfo ={
     jobName,
     jobSkill,
     jobRemote,
-    interviewStatus : 'Applied',
+    interviewStatus : 'Interview',
     jobDiscription,
    }
    
@@ -98,13 +128,72 @@ mainContainer.addEventListener('click', function(event){
 
  if(!interviewApplied){
     interViewList.push(jobInfo);
+
  }
  console.log(interViewList);
 
- renderInterview();
+ rejectedList = rejectedList.filter(item=> item.jobName !== jobInfo.jobName );
+ if(currentStatus == 'rejected' ){
+  renderRejected()
+ }
+
+
+
+ 
+ calculateCount() ;
+
+} else if(event.target.id === 'rejected-btn'){
+
+     const parentNode = event.target.parentNode.parentNode;
+   const jobName = parentNode.querySelector('#job-name').innerText;
+   const jobSkill = parentNode.querySelector('#skill').innerText;
+   const jobRemote= parentNode.querySelector('#remote-job').innerText;
+   const interviewStatus = parentNode.querySelector('#status').innerText;
+   const jobDiscription = parentNode.querySelector('#job-discription').innerText;
+
+   parentNode.querySelector('#status').innerText='Rejected';
+
+    
+
+   const jobInfo ={
+    jobName,
+    jobSkill,
+    jobRemote,
+    interviewStatus : 'Rejected',
+    jobDiscription,
+   }
+   
+//    console.log(jobInfo);
+
+// find jobname eyta ase kina na thakle push korbo 
+
+ const interviewApplied = rejectedList.find(item=> item.jobName === jobInfo.jobName)
+
+
+ if(!interviewApplied){
+    rejectedList.push(jobInfo);
+
+ }
+ console.log(rejectedList);
+
+ interViewList = interViewList.filter(item=> item.jobName !== jobInfo.jobName )
+
+ if(currentStatus == 'interview' ){
+    renderInterview();
+  
+ }
+
+ 
  calculateCount() ;
 
 }
+
+
+
+
+
+
+
 })
 
 // element create for interview and rejected section
@@ -133,6 +222,60 @@ function renderInterview(){
 
                    <p id="job-discription" class="text-[#323B49]">
                     ${interview.jobDiscription}
+                   </p>
+
+
+                   <div class=" flex gap-4 mt-4">
+                    <button id="interview-btn" class="btn btn-outline btn-success">Interview</button>
+                    <button id="rejected-btn" class="btn btn-outline btn-error">Rejected</button>
+                 </div>
+
+                
+            </div>
+
+             <div>
+            <button>
+               <a href="#"> <i class="fa-regular fa-trash-can"></i></a>
+            </button>
+        </div>
+        
+        
+        
+        
+        
+        `
+
+        filterSection.appendChild(div);
+
+
+    }}
+
+
+
+    function renderRejected(){
+
+    filterSection.innerHTML ='';
+
+    for( let rejected of rejectedList){
+
+        let div = document.createElement('div');
+        div.className =  'flex justify-between border rounded-md bg-primary-content p-8';
+
+        div.innerHTML = `
+
+         <div class="space-y-6 ">
+              
+
+                    <div>
+                     <h2 id="job-name" class="text-[#002C5C] font-bold tex-xl">${rejected.jobName}</h2>
+                    <p id="skill"  class="text-[#64748B] font-semibold text-[16px]">${rejected.jobSkill}</p>
+                    <p id="remote-job" class="text-[#64748B]  tex-[5px] mt-4">${rejected.jobRemote}</p>
+                   </div>
+
+                   <p id="status" class="text-[#002C5C]  bg-[#EEF4FF] w-fit p-3 rounded-md my-5">${rejected.interviewStatus}</p>
+
+                   <p id="job-discription" class="text-[#323B49]">
+                    ${rejected.jobDiscription}
                    </p>
 
 
